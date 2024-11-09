8e63df05f94ebf0cd02445e45b989e0fed83adff
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-09 08:11:02 +0800

    Configure Tailwind CSS and initial project styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        youngSerif: ['"Young Serif"', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      stone: {
+        100: 'hsl(30, 54%, 90%)',
+        150: 'hsl(30, 18%, 87%)',
+        600: 'hsl(30, 10%, 34%)',
+        900: 'hsl(24, 5%, 18%)',
+      },
+      brown: 'hsl(14, 45%, 36%)',
+      rose: {
+        800: 'hsl(332, 51%, 32%)',
+        50: 'hsl(330, 100%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
+
+/*
+- Mobile: 375px
+- Desktop: 1440px
+
+- Family: [Young Serif](https://fonts.google.com/specimen/Young+Serif)
+- Weights: 400
+
+- Family: [Outfit](https://fonts.google.com/specimen/Outfit)
+- Weights: 400, 600, 700
+*/
